9ffe7581af71bd26fcd4e0b14f287135f07d1cdf
Lautaro Demonte <lauty.d.p@gmail.com>
2023-08-23 19:22:43 -0300

    limpio logs para el purchase del cart

--- src/DAL/DAOs/mongoDAOs/ticketManagerMongo.js
+++ src/DAL/DAOs/mongoDAOs/ticketManagerMongo.js
@@ -2,9 +2,7 @@ import { ticketsModel } from "../../mongoDb/models/tickets.model.js";
 
 class TicketManager {
   async createTicket(obj) {
-    console.log("obj desde manager", obj);
     const ticketM = await ticketsModel.create(obj);
-    console.log("ticket desde Manager", ticketM);
     return ticketM;
   }
 }
